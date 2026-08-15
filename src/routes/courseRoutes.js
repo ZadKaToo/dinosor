7613288b2c77277ba926d7823c654ce55345e3ac
Gumsao <94772018+ZadKaToo@users.noreply.github.com>
2026-08-15 18:17:10 +0700

    Delete src directory

--- src/routes/courseRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const courseController = require('../controllers/courseController');
-const verifyToken = require('../middlewares/auth');
-
-// กำหนด URL สำหรับดึงหน้า Feed (ต้องล็อคอินก่อน)
-router.get('/feed', verifyToken, courseController.getFeed);
-
-module.exports = router;
